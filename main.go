6e1d177f673fe2c4209859be0f0aea0bd183822d
fb0801 <fbhatti0801@gmail.com>
2023-09-15 19:33:47 +0100

    get request

--- main.go
+++ main.go
@@ -19,3 +19,13 @@ var books = []book{
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
+
+func getBooks(c *gin.Context){
+	c.IndentedJSON(http.StatusOK, books)
+}
+
+func main(){
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
+}
